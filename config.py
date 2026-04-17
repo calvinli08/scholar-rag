b4c38e54b68a172d17c0ea03adeeb431983c6e30
Calvin Li <cli.calvinli@gmail.com>
2026-04-17 05:13:39 +0000

    Add DEBUG env variable

--- config.py
+++ config.py
@@ -104,6 +104,7 @@ class Settings(BaseSettings):
     app_host: str = "0.0.0.0"
     app_port: int = 8000
     log_level: str = "info"
+    debug: bool = True
 
     # -- Ingestion --
     upload_dir: str = "./uploads"
